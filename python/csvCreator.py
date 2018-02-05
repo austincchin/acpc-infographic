6979367ff1024231fb6b3771e7d35c0eb657092e
austincchin <ebi888@optonline.net>
2018-02-05 00:22:37 -0500

    addition to csv parser to also keep track of basic statistics

--- python/csvCreator.py
+++ python/csvCreator.py
@@ -1,3 +1,21 @@
+def dictAppend(d,key):
+	if key in d:
+		d[key] += 1
+	else:
+		d[key] = 1
+
+def printDic(d):
+	for k in d:
+		print "\t" + k + ":\t" + str(d[k])
+
+def writeDic(d,fname):
+	f = open(fname,"w").close()
+	f = open(fname,"w")
+	for k in d:
+		f.write( k +","+ str(d[k]) + "\n" )
+	f.close()
+
+
 if __name__ == "__main__":
 	#open csv file
 	#sort into essence/material groupings
@@ -8,9 +26,17 @@ if __name__ == "__main__":
 	outputNames = []
 	outFilelist = []
 
+	#dictionary for statistics
+	total = 0
+	per = {}
+	mat = {}
+	ess = {}
+
+
 	for i in inputFile:
 		i = i.strip()
 		j = i.split(',')
+		personality = j[1]
 		essence = j[4].strip()
 		material = j[3].strip().capitalize()
 		outFile = "../data/" + essence + material + ".csv"
@@ -27,6 +53,27 @@ if __name__ == "__main__":
 		#raw_input("press enter to continue")
 		output.write(i + "\n")
 		output.close()
+
+		total += 1
+		dictAppend(per,personality)
+		dictAppend(ess,essence)
+		dictAppend(mat,material)
+
+
+	print "Personalities"
+	printDic(per)
+	
+	print "\nMaterials"
+	printDic(mat)
+
+	print "\nEssences"
+	printDic(ess)
+
+	writeDic(per,'../data/statisticsPer')
+	writeDic(mat,'../data/statisticsMat')
+	writeDic(ess,'../data/statisticsEss')
+
+
 
 	inputFile.close()
 
