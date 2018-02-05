8fbb05a80d2b8a77ef559202aeb7fdf63d804a2a
austincchin <ebi888@optonline.net>
2018-02-05 02:45:13 -0500

    css fixes, table background colors set to help with readability

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -537,58 +537,5 @@ app.controller('MainController',
 				img: 'img/villager/Tad.png'
 			}
 		];
-
-
-		$scope.animals = [
-	  	{
-	  		name: 'Goldie',
-	  		personality: 'normal',
-	  		species: 'dog',
-	  		material: 'steel',
-	  		essence: 'natural',
-	  		img: 'img/villager/Goldie.png'
-	  	},
-	   	{
-	  		name: 'Mitzi',
-	  		personality: 'normal',
-	  		species: 'cat',
-	  		material: 'wood',
-	  		essence: 'rustic',
-	  		img: 'img/villager/Mitzi.png'
-	  	},
-	  	{
-	  		name: 'Cherry',
-	  		personality: 'uchi',
-	  		species: 'dog',
-	  		material: 'steel',
-	  		essence: 'cool',
-	  		img: 'img/villager/Cherry.png'
-	  	},
-	  	{
-	  		name: 'Stella',
-	  		personality: 'normal',
-	  		species: 'sheep',
-	  		material: 'paper',
-	  		essence: 'cute',
-	  		img: 'img/villager/Stella.png'
-	  	},
-	  	{
-	  		name: 'Kid Cat',
-	  		personality: 'jock',
-	  		species: 'cat',
-	  		material: 'wood',
-	  		essence: 'sporty',
-	  		img: 'img/villager/Kid_Cat.png'
-	  	},
-	  	{
-	  		name: 'Stitches',
-	  		personality: 'lazy',
-	  		species: 'cub',
-	  		material: 'steel',
-	  		essence: 'hip',
-	  		img: 'img/villager/Stitches.png'
-	  	}  	  	 	 	
-	  ];
-
 	 
 }]);
